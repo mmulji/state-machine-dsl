f69191748b1d5024b060767fd09c07a710c2a9e6
Milan Mulji <milan.mulji@mmedia-cs.com>
2015-08-24 08:54:57 +0100

    Renamed internal members with prefix _, so that expressive names can be used externally

--- dsl/src/main/scala/com/github/mmulji/statemachine/DSL.scala
+++ dsl/src/main/scala/com/github/mmulji/statemachine/DSL.scala
@@ -5,28 +5,32 @@ object DSL {
   case class State(name: String)
   case class Message(name: String)
 
-  implicit def addStateMachine = new StateMachine()
+  implicit def addStateMachine = {
+    val sm = new StateMachine()
+    sm
+  }
+
   implicit def addTransition = new Transition
   implicit def addStates = new States
 
   class StateMachine {
 
-   var name: String = ""
-   var states: Seq[State] = null
-   var transitions: Seq[Transition] = null
+   var _name: String = ""
+   var _states: Seq[State] = null
+   var _transitions: Seq[Transition] = null
 
    def called (i: String) = {
-     name = i
+     _name = i
      this
    }
 
    def has(i : State*) = {
-     states = i
+     _states = i
      this
    }
 
    def using(i: Transition*) = {
-     transitions = i
+     _transitions = i
      this
    }
 
@@ -36,45 +40,52 @@ object DSL {
 
   class States {
 
-   var states : Seq[State] = Seq[State]()
+   var _states : Seq[State] = Seq[State]()
 
    def addStates: States = new States
    def ~> (b: State*) = {
-     this.states = b
+     this._states = b
      this
    }
   }
 
   class Transition {
 
-   var when : (Any => Boolean) = null
-   var msg : Message = Message("")
-   var _from: State = State("")
-   var _to: State = State("")
+    type whenFn = Any => Boolean
 
-   def using(i: Message) = {
-     msg = i
+    var _when : (Any => Boolean) = null
+    var _msg : Message = Message("")
+    var _from: State = State("")
+    var _to: State = State("")
+
+    def using(i: Message) = {
+     _msg = i
      this
-   }
+    }
 
-   def from(i: State) = {
+    def from(i: State) = {
      _from = i
      this
-   }
+    }
 
-   def to(i: State) = {
+    def to(i: State) = {
      _to = i
      this
-   }
+    }
+
+    def when(x: whenFn): Transition = {
+      _when = x
+      this
+    }
 
   }
 
   class Transitions {
 
-   var transitions: Seq[Transition] = Seq[Transition]()
+   var _transitions: Seq[Transition] = Seq[Transition]()
 
    def has(i: Transition*): Transitions = {
-     transitions = i
+     _transitions = i
      this
    }
 
